1c91eaa762de284e6b88aa00d68e41d348213c26
BokChoyWithSoy <augustinejackmai@gmail.com>
2022-04-04 13:27:47 +1000

    Added Basic Sandman Functionality

--- ScoutMod/SkillStates/Scout/BatterUp.cs
+++ ScoutMod/SkillStates/Scout/BatterUp.cs
@@ -2,30 +2,78 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using RoR2;
+using RoR2.Projectile;
+using UnityEngine;
 
 namespace ScoutMod.SkillStates.Scout
 {
 	public class BatterUp : BaseSkillState
-	{
-		public static float damageCoefficient = Modules.StaticValues.sandmanDamageCoefficient;
-		public static float procCoefficient = 1.5f;
-		public static float baseDuration = 0.25f;
+    {
+        public static float damageCoefficient = 4f;
+        public static float procCoefficient = 1f;
+        public static float baseDuration = 0.75f;
+        public static float throwForce = 80f;
 
-		public float duration;
-		public float fireTime;
-		public bool hasFired;
+        private float fireTime;
+        private bool hasFired;
 
-		public override void OnEnter()
-		{
-			base.OnEnter();
-			this.duration = baseDuration / this.attackSpeedStat;
-			this.fireTime = 0.2f * this.duration;
-		}
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            this.fireTime = 0.2f;
 
-		public override void FixedUpdate()
-		{
-			base.FixedUpdate();
+            base.characterBody.SetAimTimer(2f);
+        }
 
-		}
-	}
+        public override void OnExit()
+        {
+            base.OnExit();
+        }
+
+        private void Fire()
+        {
+            if (!this.hasFired)
+            {
+                this.hasFired = true;
+
+                if (base.isAuthority)
+                {
+                    Ray aimRay = base.GetAimRay();
+
+                    ProjectileManager.instance.FireProjectile(Modules.Projectiles.ballPrefab,
+                        aimRay.origin,
+                        Util.QuaternionSafeLookRotation(aimRay.direction),
+                        base.gameObject,
+                        BatterUp.damageCoefficient * this.damageStat,
+                        0f,
+                        base.RollCrit(),
+                        DamageColorIndex.Default,
+                        null,
+                        BatterUp.throwForce);
+                }
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (base.fixedAge >= this.fireTime && !hasFired)
+            {
+                this.Fire();
+            }
+
+            if (base.fixedAge >= baseDuration && base.isAuthority)
+            {
+                this.outer.SetNextStateToMain();
+                return;
+            }
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.PrioritySkill;
+        }
+    }
 }
